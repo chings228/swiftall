ae4e7c3ac651753db9cf2e8006bed1a9e66b9806
tom <chings228@gmail.com>
2023-08-21 17:49:50 +0800

    instagram clone

---
+++ InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by man ching chan on 21/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
